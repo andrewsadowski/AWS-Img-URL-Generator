d1bb98fa3f7eedf82e2488070bd5efbd6aef16f0
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-08-30 20:42:04 -0700

    cleaned up function for gathering bucket content, removed sensitive data

--- index.js
+++ index.js
@@ -7,36 +7,30 @@ const s3 = new AWS.S3();
 const bucketParams = { Bucket: 'gallery-marquez-images' };
 
 const imageArray = [];
+const bucketContentsArr = [];
 
 s3.listObjects(bucketParams, function(err, data) {
   var bucketContents = data.Contents;
+  bucketContentsArr.push(bucketContents);
+
   for (var i = 0; i < bucketContents.length; i++) {
     var urlParams = {
       Bucket: 'gallery-marquez-images',
       Key: bucketContents[i].Key
     };
-    console.log(bucketContents[i]);
+
     s3.getSignedUrl('getObject', urlParams, function(err, url) {
-      // console.log('the url of the image is', url);
-      imageArray.push(url + '\n');
-      // console.log(imageArray);
+      imageArray.push(url);
+      let urlBreak = url + '\n';
+      fs.appendFile('./output/imageLinks.txt', urlBreak, err => {
+        if (err) throw err;
+      });
     });
   }
 });
 
-const writeDataToFile = linkArray => {
-  fs.writeFile('/output/imageLinks.txt', linkArray, err => {
-    if (err) console.log(err);
-    console.log('file written.');
+const writeDataToFile = link => {
+  fs.writeFile('./output/imageLinks.txt', link, err => {
+    if (err) throw err;
   });
 };
-
-// // const s3bucket = new S3({
-// //   params: { Bucket: 'gallery-marquez-images' }
-// // });
-
-// s3Bucket.getSignedUrl('getObject', urlParams, function(err, url) {
-//   console.log('the url of the image is', url);
-// });
-
-// console.log(s3bucket);

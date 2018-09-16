02645c87b2329b846765cabfdbeefa15f15f87f4
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-09-15 19:00:01 -0700

    added utility script to unsign urls created by previous script

---
+++ utils/formatLinks.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+/**
+ * Function that reads the output of getImageLinks,
+ *  - Saves the links in an array
+ *  - Removes the signing elements of the URL
+ *  - Returns an array of unsigned URLS
+ */
+
+const unsignImageLinks = () => {
+  //Creates the link array
+  const linkArr = fs
+    .readFileSync('output/imageLinks.txt')
+    .toString()
+    .split('\n');
+  //Unsigns the the links
+  const unsignURL = linkArray => {
+    let cleanUrls = [];
+    let regEx = /\?.*$/g;
+    linkArray.forEach(link => {
+      let cleanLink = link.replace(regEx, '');
+      console.log(cleanLink);
+      cleanUrls.push(cleanLink);
+    });
+    return cleanUrls;
+  };
+  //Save output of unsignURL in variable
+  const unsignedURLs = unsignURL(linkArr);
+
+  return unsignedURLs;
+};
+
+console.log(unsignImageLinks());
